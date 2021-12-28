b8df622019ad825e9d7623439a32ed04df55a74b
Jeffrey <jeffrey@paarhu.is>
2021-12-28 15:46:01 +0100

    initial

---
+++ src/model.ext.lua
@@ -0,0 +1,18 @@
+am.app.set_model(
+    {
+        DAEMON_CONFIGURATION = {
+            server = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
+            listen = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
+            masternodeprivkey = am.app.get_configuration("NODE_PRIVKEY"),
+            masternode = am.app.get_configuration("NODE_PRIVKEY") and 1 or nil
+        },
+        DAEMON_URL = "https://github.com/Deviantcoin/wallet-5.x/releases/download/v5.0.4/deviant-5.0.4-x86_64-linux-gnu.tar.gz",
+        DAEMON_ARCHIVE_KIND = "tar.gz",
+        DAEMON_NAME = "bin/deviantd",
+        CLI_NAME = "bin/deviant-cli",
+        CONF_NAME = "deviant.conf",
+        CONF_SOURCE = "__btc/assets/daemon.conf",
+        SERVICE_NAME = "deviantd",
+    },
+    { merge = true, overwrite = true }
+)
