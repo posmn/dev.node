0e2f0595b62b9b44a852dd33f145fd6bda2f83a5
jpaarhuis <jeffrey@paarhu.is>
2022-05-02 19:04:34 +0200

    Update model.ext.lua

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -7,6 +7,7 @@ am.app.set_model(
             masternode = am.app.get_configuration("NODE_PRIVKEY") and 1 or nil
         },
         DAEMON_URL = "https://github.com/Deviantcoin/wallet-5.x/releases/download/v6.0.0/deviant-6.0.0-x86_64-linux-gnu.tar.gz",
+        DAEMON_ARCHIVE_KIND = "tar.gz",
         DAEMON_NAME = "deviant-6.0.0/bin/deviantd",
         CLI_NAME = "deviant-6.0.0/bin/deviant-cli",
         CONF_NAME = "dev.conf",
